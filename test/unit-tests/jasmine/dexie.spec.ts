0499b8897600aec4460f28e2115037bcee73e817
Pim Vermeer <pim@zzapps.nl>
2020-03-06 15:46:35 +0100

    test(html node): bundle tests that should load in diff environments

--- test/unit-tests/jasmine/dexie.spec.ts
+++ test/unit-tests/jasmine/dexie.spec.ts
@@ -1,4 +1,7 @@
 import { Dexie as DexieImport } from 'dexie';
+import { populate } from '../../../src/index';
+
+declare interface DexiePopulateAddon { populate: typeof populate; }
 
 /*
  * Lib is not really meant for node but package should be able to be required in node.
@@ -6,11 +9,19 @@ import { Dexie as DexieImport } from 'dexie';
 describe('Dexie', () => {
     describe('Node require', () => {
         let DexieReq: typeof DexieImport;
+        let PopulateReq: DexiePopulateAddon;
         beforeAll(() => {
             DexieReq = require('dexie');
+            PopulateReq = require('../../../dist/index');
         });
-        it('should load Dexie.js', () => {
+        it('should be able to require Dexie.js', () => {
             expect(DexieReq).toBeTruthy();
+        });
+        it('should be able to require PopulateAddon', () => {
+            expect(PopulateReq).toBeTruthy();
+        });
+        it('should be able to use the PopulateAddon', () => {
+            expect(PopulateReq.populate).toBeTruthy();
         });
     });
 });
--- test/unit-tests/karma/html.spec.ts
+++ test/unit-tests/karma/html.spec.ts
@@ -1,102 +1,95 @@
-// import DexieType from 'dexie';
-// import * as rxjsImport from 'rxjs';
-// import * as rxjsOperators from 'rxjs/operators';
-// import { dexieRxjs } from '../../../src/index';
-// import { databasesPositive, Friend, methods, mockFriends } from '../../mocks/mocks';
+// tslint:disable: no-non-null-assertion
+import DexieType from 'dexie';
+import { populate } from '../../../src/index';
+import { Friend, methodsPositive, mockFriends } from '../../mocks/mocks';
 
-// declare interface DexieRxjsAddon { dexieRxjs: typeof dexieRxjs; }
-// declare const Dexie: typeof DexieType;
-// declare const rxjs: typeof rxjsImport & { operators: typeof rxjsOperators };
-// declare const DexieRxjsAddon: DexieRxjsAddon;
+declare interface DexiePopulateAddon { populate: typeof populate; }
+declare const Dexie: typeof DexieType;
+declare const DexiePopulateAddon: DexiePopulateAddon;
 
-// describe('HTML script tag', () => {
-//     beforeAll(async () => {
-//         await Promise.all([
-//             await new Promise(resolve => {
-//                 const script = document.createElement('script');
-//                 script.src = 'https://unpkg.com/dexie@latest/dist/dexie.js';
-//                 script.type = 'text/javascript';
-//                 script.onload = () => resolve();
-//                 document.head.append(script);
-//             }),
-//             await new Promise(resolve => {
-//                 const script = document.createElement('script');
-//                 script.src = 'https://unpkg.com/rxjs/bundles/rxjs.umd.min.js';
-//                 script.type = 'text/javascript';
-//                 script.onload = () => resolve();
-//                 document.head.append(script);
-//             })
-//         ]);
-//         await new Promise(resolve => {
-//             const script = document.createElement('script');
-//             script.src = `/base/dist/dexie-rxjs-addon.min.js`;
-//             script.type = 'text/javascript';
-//             script.onload = () => resolve();
-//             document.head.append(script);
-//         });
-//     });
-//     it('should load Dexie.js', () => {
-//         expect(Dexie).toBeTruthy();
-//     });
-//     it('should load RxJs', () => {
-//         expect(rxjs).toBeTruthy();
-//         expect(rxjs.operators).toBeTruthy();
-//     });
-//     it('should load the addon', () => {
-//         expect(DexieRxjsAddon).toBeTruthy();
-//         expect(DexieRxjsAddon.dexieRxjs).toBeTruthy();
-//     });
-//     databasesPositive.forEach((database, _i) => {
-//         // if (_i !== 2) { return; }
-//         describe(database.desc, () => {
-//             let db: ReturnType<typeof database.db>;
-//             let subs: rxjsImport.Subscription;
-//             beforeEach(async () => {
-//                 subs = new rxjs.Subscription();
-//                 db = database.db(Dexie);
-//                 await db.open();
-//                 expect(db.isOpen()).toBeTrue();
-//             });
-//             afterEach(async () => {
-//                 subs.unsubscribe();
-//                 await db.delete();
-//             });
-//             it('should be able to use normally', async () => {
-//                 const [friend] = mockFriends(1);
-//                 const id = await db.friends.add(friend);
-//                 const getFriend = await db.friends.get(id);
-//                 expect(getFriend).toEqual(friend);
-//                 await db.delete();
-//             });
-//             describe('Methods', () => {
-//                 methods.forEach(method => {
-//                     let friend: Friend;
-//                     let id: number;
-//                     let method$: ReturnType<typeof method.method>;
-//                     let obs$: ReturnType<ReturnType<typeof method.method>>;
+type HTMLDb = DexieType & { friends: DexieType.Table<OmitMethods<Friend>, number> };
 
-//                     const addFriend = (friendToAdd: Friend) => db.friends.add(friendToAdd)
-//                         .then(newId => {
-//                             switch (database.desc) {
-//                                 case 'TestDatabaseNoKey': return method.desc === 'Table.$' ? friendToAdd.customId : newId;
-//                                 default: return newId;
-//                             }
-//                         });
+describe('HTML script tag', () => {
+    beforeAll(async () => {
+        await Promise.all([
+            await new Promise(resolve => {
+                const script = document.createElement('script');
+                script.src = 'https://unpkg.com/dexie@next/dist/dexie.js';
+                script.type = 'text/javascript';
+                script.onload = () => resolve();
+                document.head.append(script);
+            })
+        ]);
+        await new Promise(resolve => {
+            const script = document.createElement('script');
+            script.src = `/base/dist/dexie-populate-addon.min.js`;
+            script.type = 'text/javascript';
+            script.onload = () => resolve();
+            document.head.append(script);
+        });
+    });
+    it('should load Dexie.js', () => {
+        expect(Dexie).toBeTruthy();
+    });
+    it('should load the addon', () => {
+        expect(DexiePopulateAddon).toBeTruthy();
+        expect(DexiePopulateAddon.populate).toBeTruthy();
+    });
+    describe('Database HTML', () => {
+        let db: HTMLDb;
+        beforeEach(async () => {
+            db = new Dexie('TestDatabaseHTML', {
+                addons: [DexiePopulateAddon.populate]
+            }) as HTMLDb;
+            db.version(1).stores({
+                friends: '++id, customId, firstName, lastName, shoeSize, age, hasFriends => friends.id, memberOf => clubs.id, group => groups.id, hairColor => hairColors.id',
+                clubs: '++id, name, theme => themes.id',
+                themes: '++id, name, style => styles.id',
+                styles: '++id, name, color',
+                groups: '++id, name',
+                hairColors: '++id, name'
+            });
+            await db.open();
+            expect(db.isOpen()).toBeTrue();
+        });
+        afterEach(async () => {
+            await db.delete();
+        });
+        it('should be able to use normally', async () => {
+            const [friend] = mockFriends(1);
+            const id = await db.friends.add(friend);
+            const getFriend = await db.friends.get(id);
+            expect({ ...getFriend }).toEqual({ ...friend });
+        });
+        describe('Methods', () => {
+            methodsPositive.forEach((_method, _j) => {
+                if (_j !== 0) { return; }
+                let hasFriends: OmitMethods<Friend>[];
+                let friend: OmitMethods<Friend>;
+                let id: number;
+                let hasFriendsIds: number[];
+                let method: ReturnType<typeof _method.method>;
 
-//                     describe(method.desc, () => {
-//                         beforeEach(async () => {
-//                             friend = mockFriends(1)[0];
-//                             id = await addFriend(friend);
-//                             method$ = method.method(db);
-//                             obs$ = method$(id);
-//                         });
-//                         it('should be able to use observables', async () => {
-//                             const getFriend = await obs$.pipe(rxjs.operators.take(1)).toPromise();
-//                             expect(getFriend).toEqual(friend);
-//                         });
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
+                describe(_method.desc, () => {
+                    beforeEach(async () => {
+                        const friends = mockFriends();
+                        [friend, ...hasFriends] = friends;
+                        friend = { ...friend };
+                        id = await db.friends.add(friend);
+                        hasFriendsIds = await db.friends.bulkAdd(hasFriends, { allKeys: true });
+                        hasFriends = hasFriends.map((x, i) => ({ ...x, id: hasFriendsIds[i] }));
+                        db.friends.update(id, {
+                            hasFriends: hasFriendsIds
+                        });
+                        method = _method.method(db as any);
+                    });
+                    it('should be able to use populate()', async () => {
+                        const getFriend = await method(id);
+                        const expected = { ...friend, hasFriends };
+                        expect(getFriend).toEqual(expected as any);
+                    });
+                });
+            });
+        });
+    });
+});

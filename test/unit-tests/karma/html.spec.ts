348ba8c0e04a17aa99d52b555afa5c2f6a82f49b
Pim Vermeer <pim.vermeer@gmail.com>
2020-11-15 13:56:57 +0100

    test: html script tag now uses latest Dexie.js

--- test/unit-tests/karma/html.spec.ts
+++ test/unit-tests/karma/html.spec.ts
@@ -15,7 +15,7 @@ describe('HTML script tag (Still using dexie@next HTML import)', () => {
             await new Promise(resolve => {
                 const script = document.createElement('script');
                 console.warn('Still using dexie@next HTML import !!!!!!!!!!!!!!!!!!!!!!!!');
-                script.src = 'https://unpkg.com/dexie@next/dist/dexie.js';
+                script.src = 'https://unpkg.com/dexie/dist/dexie.js';
                 script.type = 'text/javascript';
                 script.onload = () => resolve();
                 document.head.append(script);
